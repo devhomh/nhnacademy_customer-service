64d0b9512239cff8b528332f03be794024607098
devhomh <jinwoo7654@naver.com>
2024-01-15 03:43:08 +0900

    import 정리

--- src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.MessageSourceAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
