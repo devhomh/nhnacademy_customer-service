fc635e48655e8a52b3f154c20e5b9c1f5cc8a9c5
devhomh <jinwoo7654@naver.com>
2024-01-15 02:05:19 +0900

    PageController로 합병

--- src/main/java/com/nhnacademy/springmvc/controller/PageController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/PageController.java
@@ -4,24 +4,25 @@ import com.nhnacademy.springmvc.domain.Inquiry;
 import com.nhnacademy.springmvc.domain.User;
 import com.nhnacademy.springmvc.repository.InquiryRepository;
 import com.nhnacademy.springmvc.repository.UserRepository;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/admin")
-public class AdminController {
+public class PageController {
     private final UserRepository userRepository;
     private final InquiryRepository inquiryRepository;
 
-    public AdminController(UserRepository userRepository, InquiryRepository inquiryRepository) {
+    public PageController(UserRepository userRepository, InquiryRepository inquiryRepository) {
         this.userRepository = userRepository;
         this.inquiryRepository = inquiryRepository;
     }
@@ -33,7 +34,20 @@ public class AdminController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/")
+    public String home(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        User loginUser = (User) session.getAttribute("login");
+        if(Objects.isNull(loginUser)){
+            return "thymeleaf/login";
+        }
+
+        return loginUser.getAuth() == User.Auth.ROLE_ADMIN
+                ? "redirect:/admin/" + loginUser.getId()
+                : "redirect:/user/" + loginUser.getId();
+    }
+
+    @GetMapping("/admin/{userId}")
     public String adminPage(@PathVariable("userId") String id,
                             @RequestParam(value = "type", defaultValue = "칭찬") String type,
                             ModelMap modelMap){
@@ -54,5 +68,29 @@ public class AdminController {
         modelMap.addAttribute("inquiryList", inquiryList);
 
         return "thymeleaf/adminPage";
+    }
+
+    @GetMapping(value = "/user/{userId}")
+    public String userPage(@PathVariable("userId") String id,
+                           @RequestParam(value = "type", defaultValue = "칭찬") String type,
+                           ModelMap modelMap){
+        User user = userRepository.getUser(id);
+        modelMap.addAttribute("user", user);
+
+        List<Inquiry> inquiryList = inquiryRepository
+                .findAll()
+                .keySet()
+                .stream()
+                .map(inquiryRepository::getInquiry)
+                .filter(inquiry -> Objects.equals(inquiry.getUserId(), id))
+                .filter(inquiry -> Objects.equals(inquiry.getType().value(), type))
+                .collect(Collectors.toList());
+
+        Collections.reverse(inquiryList);
+        modelMap.addAttribute("selectedType", type);
+        modelMap.addAttribute("typeList", getTypeEnumList());
+        modelMap.addAttribute("inquiryList", inquiryList);
+
+        return "thymeleaf/userPage";
     }
 }
